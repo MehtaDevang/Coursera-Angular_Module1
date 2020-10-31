cf8ff0f36de5b4a11cefa8cb6abd2b07df90510a
Devang Mehta <32339538+MehtaDevang@users.noreply.github.com>
2020-10-31 19:40:50 +0530

    ShoppingListService added

--- app.js
+++ app.js
@@ -2,9 +2,55 @@
   "use strict";
   angular
     .module("LunchCheck", [])
-    .controller("LunchCheckController", LunchCheckController);
+    .controller("LunchCheckController", LunchCheckController)
+    .controller("ItemAdderController", ItemAdderController)
+    .controller("ListShowController", ListShowController)
+    .service("ShoppingListService", ShoppingListService);
 
   LunchCheckController.$inject = ["$scope"];
+  ItemAdderController.$inject = ["ShoppingListService"];
+  ListShowController.$inject = ["ShoppingListService"];
+
+  function ListShowController(ShoppingListService) {
+    var showList = this;
+    showList.items = ShoppingListService.getItems();
+
+    showList.removeItem = function (itemIndex) {
+      ShoppingListService.removeItem(itemIndex);
+    };
+  }
+  function ItemAdderController(ShoppingListService) {
+    var itemAdder = this;
+    itemAdder.itemName = "";
+    itemAdder.itemQuantity = 0;
+
+    itemAdder.addItem = function () {
+      ShoppingListService.addItem(itemAdder.itemName, itemAdder.itemQuantity);
+      itemAdder.itemName = "";
+      itemAdder.itemQuantity = 0;
+    };
+  }
+
+  function ShoppingListService() {
+    var service = this;
+    var items = [];
+
+    service.addItem = function (name, quantity) {
+      var item = {
+        name: name,
+        quantity: quantity
+      };
+      items.push(item);
+    };
+
+    service.getItems = function () {
+      return items;
+    };
+
+    service.removeItem = function (index) {
+      items.splice(index, 1);
+    };
+  }
   function LunchCheckController($scope) {
     $scope.food = "";
     $scope.message = "";
